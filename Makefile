89bbe6cf2fd2500394984627723b7c4779b34183
hamsterTears <katen-91@mail.ru>
2023-03-08 22:21:20 +0300

    vodified

--- Makefile
+++ Makefile
@@ -4,5 +4,17 @@ install:
 publish: 
 	npm publish --dry-run
 
+lint:
+	npx eslint .	
+
+fix:
+	npx eslint . --fix
+
+
 gendiff:
 	node bin/gendiff.js -h
+
+run: 
+	node bin/gendiff.js ./__fixtures__/file1.json ./__fixtures__/file2.json
+
+
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,19 @@
 #!/usr/bin/env node
 
-import { program } from 'commander';
+import { Command } from 'commander';
+// eslint-disable-next-line import/extensions
+import genDiff from '../src/index.js';
+
+const program = new Command();
 
 program
-  .version('0.0.1')
+  .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .argument('<filepath1>')
-  .argument('<filepath2>')
+  .version('0.0.1')
+  .arguments('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format')
-  .parse();
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
 
+program.parse();
