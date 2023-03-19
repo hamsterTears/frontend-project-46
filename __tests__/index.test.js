bd3fc2d970a15d323126593bbfb38a775fc85d5c
hamsterTears <katen-91@mail.ru>
2023-03-19 20:00:53 +0300

    fix test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -11,11 +11,12 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 
 const extension = ['json'];
 const expectedFile = fs.readFileSync(getFixturePath('correctFile'), 'utf-8');
-console.log(expectedFile);
+
 
 test.each(extension)('genDiff test', (ext) => {
   const file1 = getFixturePath(`file1.${ext}`);
   const file2 = getFixturePath(`file2.${ext}`);
-  expect(genDiff(file1, file2, 'json')).toStrictEqual("{\n  - follow: false,\n    host: hexlet.io,\n  - proxy: 123.234.53.22,\n  - timeout: 50,\n  + timeout: 20,\n  + verbose: true\n}");
+  expect(genDiff(file1, file2, 'json')).toStrictEqual(expectedFile);
 });
 
+// "{\n  - follow: false,\n    host: hexlet.io,\n  - proxy: 123.234.53.22,\n  - timeout: 50,\n  + timeout: 20,\n  + verbose: true\n}"
