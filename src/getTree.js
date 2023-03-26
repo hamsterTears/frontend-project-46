8c6a55a1db503882b1d4065b9c992c5679bd1016
hamsterTears <katen-91@mail.ru>
2023-03-26 18:06:55 +0300

    add sortKeys

--- src/getTree.js
+++ src/getTree.js
@@ -1,9 +1,10 @@
 import _ from 'lodash';
 
 const getTree = (data1, data2) => {
-  const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
+  const keys = _.union(Object.keys(data1), Object.keys(data2));
+  const sortKeys = _.sortBy(keys);
 
-  const result = keys.map((key) => {
+  const result = sortKeys.map((key) => {
     if (_.has(data1, key) && _.has(data2, key)) {
       if (data1[key] === data2[key]) {
         return [`  ${key}: ${data1[key]}`];
