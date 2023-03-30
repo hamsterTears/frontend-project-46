a7d55f9d4832a2d11d508a3c2a39626bc0259031
hamsterTears <katen-91@mail.ru>
2023-03-30 12:50:36 +0300

    modified

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import fs from 'fs';
-import path from 'node:path';
+import path from 'path';
 
 import getParse from './parser.js';
 import getTree from './getTree.js';
@@ -20,36 +20,3 @@ const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
   return chooseFormat(tree, formatName);
 };
 export default genDiff;
-
-// export const getFixturesPath = (filename) => path.resolve(process.cwd(), filename);
-
-// const fileFormat = (file) => {
-//   const format = path.extname(file).slice(1);
-//   return format;
-// };
-
-// const fileContent = (file) => {
-//   const content = fs.readFileSync(file, 'utf8');
-//   return content;
-// };
-
-// const dataInFile = (filepath) => {
-//   const format = fileFormat(filepath);
-//   const content = fileContent(filepath);
-//   const parsedFile = getParse(content, format);
-//   return parsedFile;
-// };
-
-// const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
-//   const tree = getTree(dataInFile(filepath1), dataInFile(filepath2));
-//   return chooseFormat(tree, formatName);
-// };
-// export default genDiff;
-
-// const genDiff = (filepath1, filepath2, format = 'stylish') => {
-//   const dataFile1 = getParse(filepath1);
-//   const dataFile2 = getParse(filepath2);
-//   const tree = getTree(dataFile1, dataFile2);
-//   return chooseFormat(tree, format);
-// };
-// export default genDiff;
